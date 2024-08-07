dce8f07b1d5e28e986847c62c32f6669c371f0b8
Chazz Bunn <darrylcbunn@gmail.com>
2024-08-07 15:00:23 -0500

    Fixed qty

--- PySparkCleanData.py
+++ PySparkCleanData.py
@@ -27,10 +27,10 @@ rdd5 = rdd4.map(lambda x: [x[0], int(x[1]), x[2], int(x[3])] + x[4:7] + [int(x[7
 rdd6 = rdd5.filter(lambda x: "/" not in x[12])
 
 # Question 1 - Part a
-rdd_q1_part_a = rdd6.map(lambda x: (x[5], 1)).reduceByKey(lambda a, b:(a+b)) # Number of sales by category
+rdd_q1_part_a = rdd6.map(lambda x: (x[5], x[7])).reduceByKey(lambda a, b:(a+b)) # Number of sales by category
 print(rdd_q1_part_a.sortBy(lambda x: -x[1]).collect())
 # Question 1 - Part b Per Country
-rdd_q1_part_b = rdd6.map(lambda x: ((x[10], x[5]), 1)).reduceByKey(lambda a, b: a+b).map(lambda x: (x[0][0], [x[0][1]] + [x[1]])).reduceByKey(lambda a, b: max(a, b, key=lambda x: x[-1]))
+rdd_q1_part_b = rdd6.map(lambda x: ((x[10], x[5]), x[7])).reduceByKey(lambda a, b: a+b).map(lambda x: (x[0][0], [x[0][1]] + [x[1]])).reduceByKey(lambda a, b: max(a, b, key=lambda x: x[-1]))
 print(rdd_q1_part_b.sortBy(lambda x: x[0]).take(5))
 
 #extract month from datetime
@@ -46,9 +46,9 @@ rdd_q2_part_b = rdd6.map(lambda x: ((x[4], extract_month(x[9]), x[10]), x[7])).r
 print(rdd_q2_part_b.sortBy(lambda x: -x[3]).collect())
 
 # Question 3 
-rdd_q3_city = rdd6.map(lambda x: (x[11], 1)).reduceByKey(lambda a, b:(a+b)) # Number of sales by city
-rdd_q3_country = rdd6.map(lambda x: (x[10], 1)).reduceByKey(lambda a, b:(a+b)) # Number of sales by country
-rdd_q3_website = rdd6.map(lambda x: (x[12], 1)).reduceByKey(lambda a, b:(a+b)) # Number of sales by website
+rdd_q3_city = rdd6.map(lambda x: (x[11], x[7])).reduceByKey(lambda a, b:(a+b)) # Number of sales by city
+rdd_q3_country = rdd6.map(lambda x: (x[10], x[7])).reduceByKey(lambda a, b:(a+b)) # Number of sales by country
+rdd_q3_website = rdd6.map(lambda x: (x[12], x[7])).reduceByKey(lambda a, b:(a+b)) # Number of sales by website
 print(rdd_q3_city.sortBy(lambda x: -x[1]).take(5))
 print(rdd_q3_country.sortBy(lambda x: -x[1]).take(5))
 print(rdd_q3_website.sortBy(lambda x: -x[1]).take(5))
@@ -60,11 +60,11 @@ def extract_hour(datetime_str):
     return dt.hour
 
 # Count sales per hour per country
-rdd_q4 = rdd6.map(lambda x: ((x[10], extract_hour(x[9])), 1)) \
+rdd_q4 = rdd6.map(lambda x: ((x[10], extract_hour(x[9])), x[7])) \
               .reduceByKey(lambda a, b: a + b)
 
 # Count sales per hour for all countries
-rdd_q4_overall = rdd6.map(lambda x: (extract_hour(x[9]), 1)) \
+rdd_q4_overall = rdd6.map(lambda x: (extract_hour(x[9]), x[7])) \
                      .reduceByKey(lambda a, b: a + b)
 
 # Sort and organize sales per hour per country
